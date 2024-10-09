c032a8c71994243c25007f348a0f5e711a848322
Nojus Stankevičius <148903463+nojusta@users.noreply.github.com>
2024-10-09 15:48:19 +0300

    5.1.

--- Pratybos_4/Pratybos4.sql
+++ Pratybos_4/Pratybos4.sql
@@ -3,7 +3,15 @@ USE adv;
 -- Scenarijus 5: Tendencijų analizė siekiant nustatyti duomenų pokyčius laikui bėgant.
 
 -- 5.1. Apskaičiuokite 3 mėn. slenkantį pardavimų vidurkį. Naudoti lentelę sales_salesorderheader.
-
+WITH MonthlySales AS (
+SELECT YEAR(OrderDate) AS OrderYear, MONTH(OrderDate) AS OrderMonth, SUM(TotalDue) AS MonthlySales
+FROM Sales_SalesOrderHeader
+GROUP BY YEAR(OrderDate), MONTH(OrderDate)
+)
+SELECT OrderYear, OrderMonth, MonthlySales,
+AVG(MonthlySales) OVER (ORDER BY OrderYear, OrderMonth ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS 'Moving Avg 3 month'
+FROM MonthlySales
+ORDER BY OrderYear, OrderMonth;
 
 -- 5.2. Paskaičiuoti pajamas pagal produktų kategorijas nuo metų pradžios iki einamos datos (angl. Year-to-Date). 
 -- Naudoti lenteles sales_salesorderheader, sales_salesorderdetail, production_product, production_productSubCategory, production_productCategory.
@@ -22,4 +30,7 @@ LEFT JOIN production_productCategory USING(ProductCategoryID)
 GROUP BY YEAR(sales_salesorderheader.OrderDate),
 MONTH(sales_salesorderheader.OrderDate), production_productCategory.name
 ORDER BY production_productCategory.name, YEAR(sales_salesorderheader.OrderDate),
-MONTH(sales_salesorderheader.OrderDate)) Paskaiciavimas
+MONTH(sales_salesorderheader.OrderDate)) Paskaiciavimas;
+
+-- Scenarijus 6: Sezoniškumo analizė siekiant nustatyti pasikartojančius ciklus, kurie reguliariais intervalais atsiranda duomenyse.
+-- 6.1. Atlikti ketvirtinę pardavimų analizę. Naudoti lenteles sales_SalesOrderHeader, sales_SalesOrderDetail.
