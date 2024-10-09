bd58670aab9f5bc52aae41fab4b81233b00da678
Nojus Stankevičius <148903463+nojusta@users.noreply.github.com>
2024-10-09 15:05:24 +0300

    4th task

---
+++ Pratybos_4/Pratybos4.sql
@@ -0,0 +1,25 @@
+USE adv;
+
+-- Scenarijus 5: Tendencijų analizė siekiant nustatyti duomenų pokyčius laikui bėgant.
+
+-- 5.1. Apskaičiuokite 3 mėn. slenkantį pardavimų vidurkį. Naudoti lentelę sales_salesorderheader.
+
+
+-- 5.2. Paskaičiuoti pajamas pagal produktų kategorijas nuo metų pradžios iki einamos datos (angl. Year-to-Date). 
+-- Naudoti lenteles sales_salesorderheader, sales_salesorderdetail, production_product, production_productSubCategory, production_productCategory.
+SELECT name, year, month, TotalRevenue,
+SUM(TotalRevenue) OVER (PARTITION BY name, year order by month) AS YTD
+FROM
+(SELECT SUM(sales_salesorderheader.TotalDue) AS TotalRevenue,
+YEAR(sales_salesorderheader.OrderDate) AS YEAR, 
+MONTH(sales_salesorderheader.OrderDate) AS Month, 
+production_productCategory.name
+FROM sales_salesorderheader
+LEFT JOIN sales_salesorderdetail USING(SalesOrderID)
+LEFT JOIN production_product USING(ProductID)
+LEFT JOIN production_productSubCategory USING(ProductSubCategoryID)
+LEFT JOIN production_productCategory USING(ProductCategoryID)
+GROUP BY YEAR(sales_salesorderheader.OrderDate),
+MONTH(sales_salesorderheader.OrderDate), production_productCategory.name
+ORDER BY production_productCategory.name, YEAR(sales_salesorderheader.OrderDate),
+MONTH(sales_salesorderheader.OrderDate)) Paskaiciavimas
